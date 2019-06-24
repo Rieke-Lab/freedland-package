b8c795ace53dcf76973d14f9835633a03cbbef08
Julian <jfreedl@outlook.com>
2019-06-24 13:50:50 -0700

    new stimulus.

---
+++ +edu/+washington/+riekelab/+freedland/+protocols/RFDiskEquivalency.m
@@ -0,0 +1,103 @@
+% Replace a natural image with an associated metamer.
+% By J. Freedland, 2019.
+classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProtocol
+    properties
+        % Stimulus timing
+        preTime = 250 % in ms
+        stimTime = 500 % in ms
+        tailTime = 250 % in ms
+        
+        % Natural image trajectory
+        imageNo = 5; % 5, 7 or 70;
+        
+        % Additional parameters
+        onlineAnalysis = 'extracellular'
+        numberOfAverages = uint16(10) % number of epochs to queue
+        amp % Output amplifier
+    end
+    
+    properties (Hidden)
+        ampType
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'}) 
+        backgroundIntensity
+    end
+
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+
+        function prepareRun(obj)
+
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.freedland.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,'splitEpoch',2); 
+            obj.showFigure('edu.washington.riekelab.freedland.figures.FrameTimingFigure',...
+                obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+        end
+        
+        function prepareEpoch(obj, epoch)
+            
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
+            device = obj.rig.getDevice(obj.amp);
+            duration = 2 * (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            
+            epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
+            epoch.addResponse(device);
+            
+            % Add metadata from Stage, makes analysis easier.
+            epoch.addParameter('canvasSize',obj.rig.getDevice('Stage').getConfigurationSetting('canvasSize'));
+            epoch.addParameter('micronsPerPixel',obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'));
+            epoch.addParameter('monitorRefreshRate',obj.rig.getDevice('Stage').getConfigurationSetting('monitorRefreshRate'));
+            epoch.addParameter('centerOffset',obj.rig.getDevice('Stage').getConfigurationSetting('centerOffset')); % in pixels
+        end
+        
+        function p = createPresentation(obj)
+            
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();             
+            p = stage.core.Presentation(2 * (obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+
+            % Set background intensity
+            p.setBackgroundColor(obj.backgroundIntensity);
+            directory = '+edu/+washington/+riekelab/+freedland/';
+            moviesDir = fullfile(fileparts(mfilename(directory)), 'Movies');
+            
+            imageString = num2str(obj.imageNo);
+            while size(imageString,2) < 3
+                imageString = strcat('0',imageString);
+            end
+            filename = strcat(directory,'img',imageString,'.mov');
+
+            scene = stage.builtin.stimuli.Movie(fullfile(moviesDir, filename));
+            scene.size = [size(obj.imageMatrix,2) * 3.3/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'),...
+                size(obj.imageMatrix,1) * 3.3/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel')];
+            p0 = canvasSize/2;
+            scene.position = p0;
+            
+            % Use linear interpolation when scaling the image
+            scene.setMinFunction(GL.LINEAR);
+            scene.setMagFunction(GL.LINEAR);
+            scene.setPlaybackSpeed = 1;
+            scene.setWrapModeS(GL.MIRRORED_REPEAT);
+            scene.setWrapModeT(GL.MIRRORED_REPEAT);
+            
+            p.addStimulus(scene);
+
+            sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(sceneVisible);
+        end
+
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+    end
+end

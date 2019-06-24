fe5b1d0f704a99842b2adba73867e5ef70d32200
Julian <jfreedl@outlook.com>
2019-06-24 14:24:44 -0700

    refined to allow movies.

--- +edu/+washington/+riekelab/+freedland/+protocols/RFDiskEquivalency.m
+++ +edu/+washington/+riekelab/+freedland/+protocols/RFDiskEquivalency.m
@@ -4,11 +4,12 @@ classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProt
     properties
         % Stimulus timing
         preTime = 250 % in ms
-        stimTime = 500 % in ms
+        stimTime = 5500 % in ms
         tailTime = 250 % in ms
         
         % Natural image trajectory
         imageNo = 5; % 5, 7 or 70;
+        playbackSpeedVal = 1; % speed to play movie (ex: 2 = 2x speed);
         
         % Additional parameters
         onlineAnalysis = 'extracellular'
@@ -20,6 +21,7 @@ classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProt
         ampType
         onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'}) 
         backgroundIntensity
+        tracker
     end
 
     methods
@@ -38,13 +40,15 @@ classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProt
                 obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,'splitEpoch',2); 
             obj.showFigure('edu.washington.riekelab.freedland.figures.FrameTimingFigure',...
                 obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+            
+            obj.tracker = 0;
         end
         
         function prepareEpoch(obj, epoch)
             
             prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
             device = obj.rig.getDevice(obj.amp);
-            duration = 2 * (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            duration = (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
             
             epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
             epoch.addResponse(device);
@@ -59,7 +63,7 @@ classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProt
         function p = createPresentation(obj)
             
             canvasSize = obj.rig.getDevice('Stage').getCanvasSize();             
-            p = stage.core.Presentation(2 * (obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
 
             % Set background intensity
             p.setBackgroundColor(obj.backgroundIntensity);
@@ -70,18 +74,24 @@ classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProt
             while size(imageString,2) < 3
                 imageString = strcat('0',imageString);
             end
-            filename = strcat(directory,'img',imageString,'.mov');
+            
+            if obj.tracker == 0
+                filename = strcat(directory,'img',imageString,'.mov'); % original trajectory
+            else
+                filename = strcat(directory,'img',imageString,'R.mov'); % alternative trajectory
+            end
 
+            % original trajectory
             scene = stage.builtin.stimuli.Movie(fullfile(moviesDir, filename));
             scene.size = [size(obj.imageMatrix,2) * 3.3/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'),...
                 size(obj.imageMatrix,1) * 3.3/obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel')];
             p0 = canvasSize/2;
             scene.position = p0;
-            
-            % Use linear interpolation when scaling the image
+
+            % settings for movies
             scene.setMinFunction(GL.LINEAR);
             scene.setMagFunction(GL.LINEAR);
-            scene.setPlaybackSpeed = 1;
+            scene.setPlaybackSpeed = obj.playbackSpeedVal;
             scene.setWrapModeS(GL.MIRRORED_REPEAT);
             scene.setWrapModeT(GL.MIRRORED_REPEAT);
             
@@ -90,6 +100,8 @@ classdef RFDiskEquivalency < edu.washington.riekelab.protocols.RiekeLabStageProt
             sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
                 @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
             p.addController(sceneVisible);
+            
+            obj.tracker = mod(obj.tracker + 1,2);
         end
 
         function tf = shouldContinuePreparingEpochs(obj)

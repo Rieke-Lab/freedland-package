23a003c363d7d94cfa49fdb5bb84e23a48912ed2
Julian <jfreedl@outlook.com>
2022-11-25 13:37:03 -0800

    added stimulus, pending rig testing.

---
+++ +edu/+washington/+riekelab/+freedland/+protocols/checkerboardContrastReversingWedges.m
@@ -0,0 +1,259 @@
+% Performs contrast-reversing stimuli in polar space
+classdef checkerboardContrastReversingWedges < edu.washington.riekelab.protocols.RiekeLabStageProtocol
+
+    properties
+        % Basic parameters
+        preTime = 250 % ms
+        stimTime = 2000 % ms
+        tailTime = 250 % ms
+
+        % Spatial parameters
+        centerDiameter = 300; % in um
+        wedges = 8; % radial wedges
+        circles = [2, 3, 4, 5]; % circular divisions
+
+        % Stimulus parameters
+        contrast = 0.9; % 0 - 1
+        temporalFrequency = 4 % Hz
+        backgroundIntensity = 0.168 % (0-1)
+
+        % Additional
+        randomizeOrder = true;
+        onlineAnalysis = 'extracellular'
+        numberOfAverages = uint16(5) % number of epochs to queue
+        amp
+    end
+
+    properties (Hidden)
+        ampType
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'})
+        masks
+        xAxis
+    end
+    
+    properties (Hidden, Transient)
+        analysisFigure
+    end
+    
+    methods
+
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.freedland.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis);
+            obj.showFigure('edu.washington.riekelab.freedland.figures.FrameTimingFigure',...
+                obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+
+            %%% M. Turner's analysis figure
+            obj.xAxis = obj.circles;
+            if length(obj.xAxis) > 1
+                colors = edu.washington.riekelab.turner.utils.pmkmp(length(obj.xAxis),'CubicYF');
+            else
+                colors = [0 0 0];
+            end
+            if ~strcmp(obj.onlineAnalysis,'none')
+                % Custom figure handler
+                if isempty(obj.analysisFigure) || ~isvalid(obj.analysisFigure)
+                    obj.analysisFigure = obj.showFigure('symphonyui.builtin.figures.CustomFigure', @obj.CRGanalysis);
+                    f = obj.analysisFigure.getFigureHandle();
+                    set(f, 'Name', 'CRGs');
+                    obj.analysisFigure.userData.trialCounts = zeros(length(obj.xAxis));
+                    obj.analysisFigure.userData.F1 = zeros(length(obj.xAxis));
+                    obj.analysisFigure.userData.F2 = zeros(length(obj.xAxis));
+                    obj.analysisFigure.userData.axesHandle = axes('Parent', f);
+                else
+                    obj.analysisFigure.userData.trialCounts = zeros(length(obj.xAxis));
+                    obj.analysisFigure.userData.F1 = zeros(length(obj.xAxis));
+                    obj.analysisFigure.userData.F2 = zeros(length(obj.xAxis));
+                end
+            end
+
+            % Starting parameters
+            centerDiameter_px = obj.rig.getDevice('Stage').um2pix(obj.centerDiameter);
+            canvasSize      = obj.rig.getDevice('Stage').getCanvasSize();
+
+            % Define polar space
+            [xx,yy] = meshgrid(1:canvasSize(1),1:canvasSize(2));
+            r = sqrt((xx - canvasSize(1)/2).^2 + (yy - canvasSize(2)/2).^2);
+            th = atan((xx - canvasSize(1)/2) ./ (yy - canvasSize(2)/2));
+            th = abs(th-pi/2);              
+            nonsmooth = find(diff(th) > pi/2,1);
+            th(1:nonsmooth,:) = th(1:nonsmooth,:) + pi;
+            th = rad2deg(th);
+            
+            obj.masks = cell(2,length(obj.circles));
+            for z = 1:length(obj.circles)
+            
+                numberCircles = obj.circles(z);
+            
+                % Identify circular masks
+                circularMasks = cell(numberCircles,1);
+                diameters = 0:centerDiameter_px/numberCircles:centerDiameter_px;
+                for a = 1:length(diameters)-1
+                    circularMasks{a,1} = (r >= (diameters(a)/2)) & (r < (diameters(a+1)/2));
+                end
+                
+                % Turn all circular masks into wedges
+                obj.wedges(obj.wedges == 0) = 1;
+                theta = 0:360/obj.wedges:360;
+                m = cell(obj.wedges,size(circularMasks,1));
+                for b = 1:size(circularMasks,1)
+                    for a = 1:length(theta) - 1
+                        radialMask = th >= theta(a) & th < theta(a+1); % Angular filter (theta)
+                        m{a,b} = radialMask .* circularMasks{b,1}; % Individual masks
+                    end
+                end
+                
+                % Build contrast-reversing stimuli
+                obj.masks{1,z} = zeros(size(xx));
+                for b = 1:size(m,1)
+                    % Build checkerboard pattern
+                    startingMask = mod(b,2)+1;
+                    for c = startingMask:2:size(m,2)
+                        obj.masks{1,z} = obj.masks{1,z} + m{b,c};
+                    end
+                end
+                obj.masks{2,z} = (r < (diameters(end)/2)) .* (obj.masks{1,z} == 0);
+            end
+
+            % Randomize order
+            obj.sequence = 1:size(obj.masks,2);
+            if obj.randomizeOrder == true
+                obj.sequence = obj.sequence(randperm(length(obj.sequence)));
+            end
+
+            obj.counter = 0;
+            obj.sequence = repmat(obj.sequence,1,obj.numberOfAverages);
+
+            %%% Quick bug fix: very last epoch won't play, so we double last condition
+            obj.sequence = [obj.sequence, obj.sequence(end,:)];
+        end
+
+        function CRGanalysis(obj, ~, epoch) % Online analysis function by M. Turner
+            response = epoch.getResponse(obj.rig.getDevice(obj.amp));
+            epochResponseTrace = response.getData();
+            sampleRate = response.sampleRate.quantityInBaseUnits;
+            
+            axesHandle = obj.analysisFigure.userData.axesHandle;
+            trialCounts = obj.analysisFigure.userData.trialCounts;
+            F1 = obj.analysisFigure.userData.F1;
+            F2 = obj.analysisFigure.userData.F2;
+            
+            if strcmp(obj.onlineAnalysis,'extracellular') %spike recording
+                %take (prePts+1:prePts+stimPts)
+                epochResponseTrace = epochResponseTrace((sampleRate*obj.preTime/1000)+1:(sampleRate*(obj.preTime + obj.stimTime)/1000));
+                %count spikes
+                S = edu.washington.riekelab.turner.utils.spikeDetectorOnline(epochResponseTrace);
+                epochResponseTrace = zeros(size(epochResponseTrace));
+                epochResponseTrace(S.sp) = 1; %spike binary
+                
+            else %intracellular - Vclamp
+                epochResponseTrace = epochResponseTrace-mean(epochResponseTrace(1:sampleRate*obj.preTime/1000)); %baseline
+                %take (prePts+1:prePts+stimPts)
+                epochResponseTrace = epochResponseTrace((sampleRate*obj.preTime/1000)+1:(sampleRate*(obj.preTime + obj.stimTime)/1000));
+            end
+
+            L = length(epochResponseTrace); %length of signal, datapoints
+            X = abs(fft(epochResponseTrace));
+            X = X(1:L/2);
+            f = sampleRate*(0:L/2-1)/L; %freq - hz
+            [~, F1ind] = min(abs(f-obj.temporalFrequency)); %find index of F1 and F2 frequencies
+            [~, F2ind] = min(abs(f-2*obj.temporalFrequency));
+
+            F1power = 2*X(F1ind); %pA^2/Hz for current rec, (spikes/sec)^2/Hz for spike rate
+            F2power = 2*X(F2ind); %double b/c of symmetry about zero
+            
+            barInd = find(obj.sequence(obj.counter) == obj.xAxis); % -1 because we already ran update
+            trialCounts(barInd) = trialCounts(barInd) + 1;
+            F1(barInd) = F1(barInd) + F1power;
+            F2(barInd) = F2(barInd) + F2power;
+            
+            cla(axesHandle);
+            h1 = line(obj.xAxis, F1./trialCounts, 'Parent', axesHandle);
+            set(h1,'Color','g','LineWidth',2,'Marker','o');
+            h2 = line(obj.xAxis, F2./trialCounts, 'Parent', axesHandle);
+            set(h2,'Color','r','LineWidth',2,'Marker','o');
+            hl = legend(axesHandle,{'F1','F2'});
+            xlabel(axesHandle,'Bar width (um)')
+            ylabel(axesHandle,'Amplitude')
+
+            obj.analysisFigure.userData.trialCounts = trialCounts;
+            obj.analysisFigure.userData.F1 = F1;
+            obj.analysisFigure.userData.F2 = F2;
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
+            
+            device = obj.rig.getDevice(obj.amp);
+            duration = (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
+            epoch.addResponse(device);
+
+            epoch.addParameter('numberOfCircles_specific', obj.circles(obj.sequence(obj.counter+1)));
+        end
+
+        function p = createPresentation(obj)
+            
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3); %create presentation of specified duration
+            p.setBackgroundColor(obj.backgroundIntensity); % Set background intensity
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+
+            specificMasks = obj.masks(:,obj.sequence(obj.counter+1));
+            
+            % Add contrast reversing gratings
+            for a = 1:2
+                grate           = stage.builtin.stimuli.Grating('square'); % Square wave grating
+                grate.size      = canvasSize;
+                grate.position  = canvasSize / 2;
+                grate.color     = 2 * obj.backgroundIntensity; % Amplitude of square wave
+                grate.contrast  = obj.contrast; % Multiplier on square wave
+                grateShape      = uint8(specificMasks{a,1}*255);
+                grateMask       = stage.core.Mask(grateShape);
+                grate.setMask(grateMask);
+            
+                % Contrasting gratings between each set of masks
+                if a == 1
+                    grate.phase = 180;
+                else
+                    grate.phase = 0;
+                end
+
+                p.addStimulus(grate); % Add grating to the presentation
+                
+                % Control contrast
+                grateContrast = stage.builtin.controllers.PropertyController(grate, 'contrast',...
+                    @(state)getGrateContrast(obj, state.time - obj.preTime/1e3));
+                p.addController(grateContrast); % Add the controller
+                
+                % Hide during pre & post
+                grateVisible = stage.builtin.controllers.PropertyController(grate, 'visible', ...
+                    @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+                p.addController(grateVisible);
+            end
+            obj.counter = obj.counter + 1;
+
+            function c = getGrateContrast(obj, time)
+                c = obj.contrast.*sin(2 * pi * obj.temporalFrequency * time);
+            end
+        end
+ 
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < (length(obj.sequence)-1); % Present one less epoch
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < (length(obj.sequence)-1);
+        end
+        
+        
+    end
+    
+end
